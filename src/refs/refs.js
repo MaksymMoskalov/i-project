1b163f951ea8f0861ccb49ca2dfbb79f62b9c84d
Maksim <max.godnavi133@gmail.com>
2023-12-03 19:44:01 +0200

    fix 1

--- src/refs/refs.js
+++
@@ -1,3 +0,0 @@
-export const formEl = document.getElementById('search-form');
-export const cardsEl = document.querySelector('.gallery');
-export const loadMoreBtn = document.querySelector('.load-more');
